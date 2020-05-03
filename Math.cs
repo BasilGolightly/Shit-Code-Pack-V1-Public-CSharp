dffe8d9ca94766f4672a92360ec59e5c7e22dc0c
BasilGolightly <64554669+BasilGolightly@users.noreply.github.com>
2020-05-03 13:43:42 +0200

    Add files via upload

---
+++ Math.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Threading;
+using Math;
+using Microsoft.VisualBasic;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool start;
+            
+            Console.WriteLine("Ali želiš vžgati program? (y/n)");
+            string začetek = Console.ReadLine();
+
+            while (start = true)
+            {
+            
+            //izbiranje operatorja
+            Console.WriteLine("Matematika se pripravlja...");
+            Console.WriteLine("Lahko uporabiš decimalke in ob tem tudi naravna št.");
+            Console.WriteLine("Katero vrsto operacij želiš uporabiti(Množenje, deljenje, plus, minus, koren)?");
+            string input = Console.ReadLine();
+
+            //zmnožek method
+            static double zmnožek(double x, double y)
+            {
+                return x * y;
+            }
+
+            //plus method
+            static double plus(double x, double y)
+            {
+                return x + y;
+            }
+
+            //minus method
+            static double minus(double x, double y)
+            {
+                return x - y;
+            }
+
+            //deljenje method
+            static double deljenje(double x , double y)
+            {
+                return x / y;
+            }
+
+            static float koren(float x)
+            {
+                    return MathF.Sqrt(x);
+
+            }
+
+
+
+                if (input == "Množenje" || input == "množenje" || input == "x" || input == "X" || input == "multiplication" || input == "MULTIPLICATION" || input == "MNOŽENJE" || input == "Multiplication" || input == "Zmnožek" || input == "zmnožek" || input == "ZMNOŽEK")
+                {
+                    //množenje
+                    Console.WriteLine("Vredu. Se pravi najprej napiši prvo številko (npr. 1 ali 15,2 ali 5,22)");
+                    //input prve za zmnožek
+                    double prva_zmnožek = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Vredu. Pravkar si vpisal št. " + prva_zmnožek + ", zdaj pa še vpiši drugo št. (npr. " + prva_zmnožek + " x 10,0)");
+                    //input druga za zmnožek
+                    double druga_zmnožek = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Rezultat je: " + zmnožek(prva_zmnožek, druga_zmnožek));
+                    Console.WriteLine("Ali želiš še kaj zračunati?(y/n)");
+                    string nadaljevanje_množenje = Console.ReadLine();
+
+                    if (nadaljevanje_množenje=="y"||nadaljevanje_množenje=="Y"||nadaljevanje_množenje=="YES"||nadaljevanje_množenje=="yes"||nadaljevanje_množenje=="Yes"||nadaljevanje_množenje=="Ja"||nadaljevanje_množenje=="ja"||nadaljevanje_množenje=="JA")
+                    {
+                        Console.WriteLine("Vredu.");
+                        continue;
+                    }
+
+                    else if (nadaljevanje_množenje=="N"||nadaljevanje_množenje=="n"||nadaljevanje_množenje=="NE"||nadaljevanje_množenje=="ne"||nadaljevanje_množenje=="Ne"||nadaljevanje_množenje=="No"||nadaljevanje_množenje=="NO"||nadaljevanje_množenje=="no")
+                    {
+                        Console.WriteLine("Vredu.");
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Ne razumem. Pošiljam te v meni za izbiro matematičnih operacij.");
+                        Console.WriteLine("Če tega ne želiš, potem napiši 'exit'.");
+                        continue;
+                    }
+                }
+
+                else if (input == "plus" || input == "PLUS" || input == "Plus" || input == "+")
+                {
+                    //plus
+
+                    Console.WriteLine("Vredu. Se pravi napiši prvo št. (npr. 1,3 ali 5 ali 16,1)");
+                    //input za prvi plus
+                    double prva_plus = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Vredu. Pravkar si vpisal št. " + prva_plus + ", zdaj pa še vpiši drugo št. (npr. " + prva_plus + " + 10,0.)");
+                    //input za drugi plus
+                    double druga_plus = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Rezultat je: " + plus(prva_plus, druga_plus));
+                }
+
+                else if (input == "minus" || input == "MINUS" || input == "Minus" || input == "-")
+                {
+                    //minus
+                    Console.WriteLine("Vredu. Se pravi najprej napiši prvo št. (npr. 1 ali 15,2 ali 5,33)");
+                    //input za prvi minus
+                    double prva_minus = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Vredu. Pravkar si vpisal št. " + prva_minus + ", zdaj pa še vpiši drugo št. (npr. " + prva_minus + " - 10,0.)");
+                    //input za drugi minus
+                    double druga_minus = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Rezultat je: " + minus(prva_minus, druga_minus));
+                }
+
+                else if (input == "Deljenje" || input == "DELJENJE" || input == "Deljenje" || input == "/")
+                {
+                    //Deljenje
+                    Console.WriteLine("Vredu. Napiši prvo št. (npr. 1,3 ali 5 ali 16,1)");
+                    //input za prvi količnik
+                    double prva_deljenje = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Vredu. Pravkar si vpisal št. " + prva_deljenje + ", zdaj pa še vpiši drugo št. (npr. " + prva_deljenje + " : 10,0)");
+                    //input za drugi količnik
+                    double druga_deljenje = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Rezultat je: " + deljenje(prva_deljenje, druga_deljenje));
+                    Console.WriteLine("Ali želiš še kaj zračunati?(y/n)");
+                    string nadaljevanje_deljenje = Console.ReadLine();
+
+                    if (nadaljevanje_deljenje=="y"||nadaljevanje_deljenje=="Y"||nadaljevanje_deljenje=="YES"||nadaljevanje_deljenje=="yes"||nadaljevanje_deljenje=="Yes"||nadaljevanje_deljenje=="Ja"||nadaljevanje_deljenje=="ja"||nadaljevanje_deljenje=="JA")
+                    {
+                        Console.WriteLine("Vredu.");
+                        continue;
+                    }
+                }
+
+
+                else if (input == "Koren" || input == "KOREN" || input == "koren" || input == "Square Root" || input == "SQRT" || input == "Sqrt" || input == "sqrt" || input == "square root" || input == "SQUARE ROOT")
+                {
+                    //koren
+                    Console.WriteLine("Vredu. Napiši št., ki jo hočeš dati pod koren (npr. 1.3 ali 5 ali 16.1)");
+                    //input za prva koren
+                    float prva_koren = float.Parse(Console.ReadLine());
+                    Console.WriteLine(prva_koren + " pod korenom je " + koren(prva_koren));
+                    Console.WriteLine("Ali želiš še kaj zračunati?(y/n)");
+                    string nadaljevanje_koren = Console.ReadLine();
+
+                    if (nadaljevanje_koren == "y" || nadaljevanje_koren == "Y" || nadaljevanje_koren == "YES" || nadaljevanje_koren == "Yes" || nadaljevanje_koren == "yes" || nadaljevanje_koren == "ja" || nadaljevanje_koren == "JA" || nadaljevanje_koren == "Ja")
+                    {
+                        Console.WriteLine("V redu.");
+                        continue;
+                    }
+
+                    else if (nadaljevanje_koren == "ne" || nadaljevanje_koren == "NE" || nadaljevanje_koren == "Ne" || nadaljevanje_koren == "n" || nadaljevanje_koren == "N" || nadaljevanje_koren == "NO" || nadaljevanje_koren == "No" || nadaljevanje_koren == "no")
+                    {
+                        Console.WriteLine("v redu.");
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Ne razumem. Pošiljam te v meni za izbiro matematičnih operacij.");
+                        Console.WriteLine("Če tega ne želiš, potem napiši v meniju 'exit'.");
+                        continue;
+                    }
+                }
+                else if (input == "exit" || input == "EXIT" || input == "Exit" || input == "Izhod" || input == "izhod" || input == "IZHOD")
+                {
+
+                }
+           
+
+            }
+
+            if (začetek=="y"||začetek=="Y"||začetek=="YES"||začetek=="yes"||začetek=="Yes"||začetek=="Ja"||začetek=="JA"||začetek=="ja")
+            {
+                Console.WriteLine("Program se zaganja...");
+                start = true;
+            }
+
+            else if (začetek=="N"||začetek=="n"||začetek=="No"||začetek=="NO"||začetek=="no"||začetek=="ne"||začetek=="NE"||začetek=="Ne")
+            {
+                Console.WriteLine("Vredu.");
+                start = false;
+            }
+
+            else
+            {
+                Console.WriteLine("Ne razumem. Pošiljam te v meni za izbiro matematičnih operacij.");
+                Console.WriteLine("Če tega ne želiš, potem napiši 'exit' pri izbiri matematičnih operacij.");
+            }
+            
+           
+           
+            
+           
+            
+            
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static double lbstokg(double x)
+            {
+                return x * 0.45359237;
+            }
+            
+            static double mtoft(double x)
+            {
+                return x * 3.2808399;
+            }
+
+            static double cmtoinch(double x)
+            {
+                return x * 0.393700787;
+            }
+
+            static double kgtolbs(double x)
+            {
+                return x / 0.45359237;
+            }
+
+            static double inchtocm(double x)
+            {
+                return x * 2.54;
+            }
+
+            static double fttom(double x)
+            {
+                return x / 3.2808399;
+            }
+            
+            bool start;
+            Console.WriteLine("Ali hočeš vžgati program za pretvorbe?(y/n)");
+            string program_input = Console.ReadLine();
+            //input za začetek
+            
+            if (program_input == "y" || program_input == "Y"|| program_input == "Yes"|| program_input=="da"||program_input=="DA"||program_input =="Da"|| program_input== "YES"|| program_input == "yes"||program_input == "ja"|| program_input == "Ja")
+            {
+                   
+                start = true;
+                   
+                   while (start == true)
+                   {
+                       //izbira pretvorbe 
+                       Console.WriteLine("Kaj želiš pretvoriti? Izberi/napiši eno od naslednjega: lbs do kg, meter do ft, cm do inch (ali obratno).");
+                       Console.WriteLine("Če ne želiš uporabiti programa, napiši 'exit'.");
+                       string izbira_pretvorbe = Console.ReadLine();
+
+                    if (izbira_pretvorbe == "lbs do kg " || izbira_pretvorbe == "LBS DO KG" || izbira_pretvorbe == "Lbs do Kg" || izbira_pretvorbe == "lbs to kg" || izbira_pretvorbe == "LBS TO KG" || izbira_pretvorbe == "Lbs to Kg")
+                    {
+
+                        // input prve št. (lbs to pounds)
+                        Console.WriteLine("V redu. Vpiši št. lbs (pounds):");
+                        double prva_lbskg = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(prva_lbskg + " lbs = " + lbstokg(prva_lbskg));
+                        //nadaljevanje input
+                        Console.WriteLine("Ali želiš še kaj pretvoriti?(y/n)");
+                        string nadaljevanje_lbskg = Console.ReadLine();
+
+                        if (nadaljevanje_lbskg == "Y" || nadaljevanje_lbskg == "y" || nadaljevanje_lbskg == "Ja" || nadaljevanje_lbskg == "JA" || nadaljevanje_lbskg == "ja" || nadaljevanje_lbskg == "Yes" || nadaljevanje_lbskg == "YES" || nadaljevanje_lbskg == "yes")
+                        {
+                            Console.WriteLine("Vredu.");
+                            continue;
+                        }
+
+                        else if (nadaljevanje_lbskg == "N" || nadaljevanje_lbskg == "n" || nadaljevanje_lbskg == "Ja" || nadaljevanje_lbskg == "JA" || nadaljevanje_lbskg == "ja" || nadaljevanje_lbskg == "yes" || nadaljevanje_lbskg == "YES" || nadaljevanje_lbskg == "Yes")
+                        {
+                            Console.WriteLine("V redu. Hvala za uporabo tega programa!");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ne razumem. Vseeno je.");
+                            Console.WriteLine("Če želiš iti ven iz programa napiši 'exit'");
+                            continue;
+                        }
+                    }
+
+                    //exit
+                    else if (izbira_pretvorbe == "Exit" || izbira_pretvorbe == "EXIT" || izbira_pretvorbe == "exit")
+                    {
+                        Console.WriteLine("V redu.");
+                        break;
+                    }
+
+                    //meter to feet
+                    else if (izbira_pretvorbe == "Meter to Feet" || izbira_pretvorbe == "METER to FEET" || izbira_pretvorbe == "meter to feet" || izbira_pretvorbe == "meter do feet" || izbira_pretvorbe == "METER DO FEET" || izbira_pretvorbe == "Meter do Feet" || izbira_pretvorbe == "m to ft" || izbira_pretvorbe == "m do ft" || izbira_pretvorbe == "M TO FT")
+                    {
+                        Console.WriteLine("V redu. Napiši št. metrov.");
+                        double prva_mtoft = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(prva_mtoft + " m = " + mtoft(prva_mtoft) + " ft.");
+                        Console.WriteLine("Ali želiš še kaj pretvoriti (y/n)");
+                        string nadaljevanje_mtoft = Console.ReadLine();
+
+                        if (nadaljevanje_mtoft == "y" || nadaljevanje_mtoft == "Y" || nadaljevanje_mtoft == "YES" || nadaljevanje_mtoft == "Yes" || nadaljevanje_mtoft == "yes" || nadaljevanje_mtoft == "Ja" || nadaljevanje_mtoft == "ja")
+                        {
+                            Console.WriteLine("Vredu.");
+                            continue;
+                        }
+
+                        else if (nadaljevanje_mtoft == "n" || nadaljevanje_mtoft == "N" || nadaljevanje_mtoft == "No" || nadaljevanje_mtoft == "no" || nadaljevanje_mtoft == "NO" || nadaljevanje_mtoft == "Ne" || nadaljevanje_mtoft == "ne" || nadaljevanje_mtoft == "NE")
+                        {
+                            Console.WriteLine("V redu.");
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ne razumem. Bom te kar dal v meni za izbiro pretvorb");
+                            Console.WriteLine("Če ne želiš potem pri izbiri napiši 'exit'.");
+                        }
+
+
+                    }
+
+                    //cm to inch
+                    else if (izbira_pretvorbe == "cm do inch" || izbira_pretvorbe == "CM DO INCH" || izbira_pretvorbe == "Cm Do Inch" || izbira_pretvorbe == "cm to inch" || izbira_pretvorbe == "CM TO INCH" || izbira_pretvorbe == "Cm To Inch")
+                    {
+                        Console.WriteLine("V redu. Napiši št. cm");
+                        double prva_cmtoinch = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(program_input + " cm = " + cmtoinch(prva_cmtoinch) + " inch.");
+                        Console.WriteLine("Ali želiš še kaj pretvoriti(y/n)");
+                        string nadaljevanje_cmtoinch = Console.ReadLine();
+
+                        if (nadaljevanje_cmtoinch == "Y" || nadaljevanje_cmtoinch == "y" || nadaljevanje_cmtoinch == "YES" || nadaljevanje_cmtoinch == "Yes" || nadaljevanje_cmtoinch == "yes" || nadaljevanje_cmtoinch == "Ja" || nadaljevanje_cmtoinch == "ja" || nadaljevanje_cmtoinch == "JA")
+                        {
+                            Console.WriteLine("V redu.");
+                            continue;
+                        }
+
+                        else if (nadaljevanje_cmtoinch == "NO" || nadaljevanje_cmtoinch == "No" || nadaljevanje_cmtoinch == "no" || nadaljevanje_cmtoinch == "n" || nadaljevanje_cmtoinch == "N" || nadaljevanje_cmtoinch == "Ne" || nadaljevanje_cmtoinch == "NE" || nadaljevanje_cmtoinch == "ne")
+                        {
+                            Console.WriteLine("V redu.");
+                            break;
+                        }
+
+                    }
+
+                    //kg to lbs
+                    else if (izbira_pretvorbe == "Kg to Lbs" || izbira_pretvorbe == "KG TO LBS" || izbira_pretvorbe == "kg to lbs" || izbira_pretvorbe == "kg do lbs" || izbira_pretvorbe == "KG DO LBS" || izbira_pretvorbe == "Kg do Lbs")
+                    {
+                        Console.WriteLine("V redu. Vpiši št. kg");
+                        double prva_kgtolbs = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(prva_kgtolbs + " kg = " + kgtolbs(prva_kgtolbs) + " lbs.");
+                        Console.WriteLine("Ali želiš še kaj pretvoriti? (y/n)");
+                        string nadaljevanje_kgtolbs = Console.ReadLine();
+
+                        if (nadaljevanje_kgtolbs == "Y" || nadaljevanje_kgtolbs == "y" || nadaljevanje_kgtolbs == "YES" || nadaljevanje_kgtolbs == "Yes" || nadaljevanje_kgtolbs == "yes" || nadaljevanje_kgtolbs == "ja" || nadaljevanje_kgtolbs == "JA" || nadaljevanje_kgtolbs == "Ja")
+                        {
+                            Console.WriteLine("V redu.");
+                            continue;
+                        }
+
+                        else if (nadaljevanje_kgtolbs=="N"||nadaljevanje_kgtolbs=="n"||nadaljevanje_kgtolbs=="Ne"||nadaljevanje_kgtolbs=="NE"||nadaljevanje_kgtolbs=="ne"||nadaljevanje_kgtolbs=="No"||nadaljevanje_kgtolbs=="NO"||nadaljevanje_kgtolbs=="no")
+                        {
+                            Console.WriteLine("V redu.");
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ne razumem. Pošiljam te v meni za izbiro pretvorb.");
+                            Console.WriteLine("Če tega ne želiš, napiši 'exit' pri izbiri za pretvorbe.");
+                            continue;
+                        }
+                    
+                    }
+
+                    //inch to cm
+                    else if (izbira_pretvorbe=="inch to cm"||izbira_pretvorbe=="INCH TO CM"||izbira_pretvorbe=="Inch To Cm"||izbira_pretvorbe=="Inch to Cm"||izbira_pretvorbe=="inch do cm"||izbira_pretvorbe=="Inch do Cm"||izbira_pretvorbe=="INCH DO CM"||izbira_pretvorbe=="Inch Do Cm")
+                    {
+                        Console.WriteLine("V redu. Vpiši št. inch.");
+                        double prva_inchtocm = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(program_input + " inch = " + inchtocm(prva_inchtocm) + " cm.");
+                        Console.WriteLine("Ali želiš še kaj pretvoriti? (y/n)");
+                        string nadaljevanje_inchtocm = Console.ReadLine();
+
+                        if (nadaljevanje_inchtocm=="y"||nadaljevanje_inchtocm=="Y"||nadaljevanje_inchtocm=="YES"||nadaljevanje_inchtocm=="Yes"||nadaljevanje_inchtocm=="yes"||nadaljevanje_inchtocm=="Ja"||nadaljevanje_inchtocm=="JA"||nadaljevanje_inchtocm=="ja")
+                        {
+                            Console.WriteLine("V redu.");
+                            continue;
+
+                        }
+
+                        else if (nadaljevanje_inchtocm == "N"||nadaljevanje_inchtocm=="n"||nadaljevanje_inchtocm=="No"||nadaljevanje_inchtocm=="NO"||nadaljevanje_inchtocm=="no"||nadaljevanje_inchtocm=="Ne"||nadaljevanje_inchtocm=="NE"||nadaljevanje_inchtocm=="ne")
+                        {
+                            Console.WriteLine("V redu.");
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ne razumem. Poslal te bom v meni za izbiro pretvorb.");
+                            Console.WriteLine("Če tega ne želiš, napiši pri ibiri 'exit', kar te bo vrglo ven iz programa.");
+                            continue;
+                        }
+                    }
+
+                    //ft to m
+                    else if (izbira_pretvorbe=="feet to meter"||izbira_pretvorbe=="Feet to Meter"||izbira_pretvorbe=="FEET TO METER"||izbira_pretvorbe=="ft to m"||izbira_pretvorbe=="FT TO M"||izbira_pretvorbe=="Ft to M"||izbira_pretvorbe=="Ft do M"||izbira_pretvorbe=="ft do m"||izbira_pretvorbe=="FT DO M"||izbira_pretvorbe=="Feet do Meter"||izbira_pretvorbe=="feet do meter"||izbira_pretvorbe=="FEET DO METER")
+                    {
+                        Console.WriteLine("V redu. Napiši št. ft");
+                        double prva_fttom = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(prva_fttom + " ft = "  + fttom(prva_fttom) + " m");
+                        Console.WriteLine("Ali želiš še kaj pretvoriti?");
+                        string nadaljevanje_fttom = Console.ReadLine();
+
+                        if (nadaljevanje_fttom=="y"||nadaljevanje_fttom=="Y"||nadaljevanje_fttom=="Yes"||nadaljevanje_fttom=="yes"||nadaljevanje_fttom=="YES"||nadaljevanje_fttom=="Ja"||nadaljevanje_fttom=="JA"||nadaljevanje_fttom=="ja")
+                        {
+                            Console.WriteLine("V redu.");
+                            continue;
+                        }
+
+                        else if (nadaljevanje_fttom=="N"||nadaljevanje_fttom=="n"||nadaljevanje_fttom=="NO"||nadaljevanje_fttom=="no"||nadaljevanje_fttom=="No"||nadaljevanje_fttom=="Ne"||nadaljevanje_fttom=="NE"||nadaljevanje_fttom=="ne")
+                        {
+                            Console.WriteLine("V redu.");
+                            break;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Ne razumem. Pošiljam te v meni za izbiro pretvorb.");
+                            Console.WriteLine("Če tega ne želiš, napiši 'exit', ko izbiraš pretvorbe.");
+                            continue;
+                        }
+                        
+                    }
+                     
+                   }     
+            }       
+                        
+            // zavrni igro
+            else if (program_input == "N"||program_input=="n"||program_input=="No"||program_input=="NO"||program_input=="no"||program_input=="Ne"|| program_input=="NE"||program_input=="ne")
+            {
+                start = false;
+            }
+            
+            //exit
+            else if (program_input == "Exit"|| program_input=="exit"|| program_input=="EXIT")
+            {
+                start = false;
+                Console.WriteLine("V redu.");
+            }    
+            
+
+            // nerazumljiv input
+            else
+            {
+                Console.WriteLine("Ne razumem. Pošiljam te v meni za izbiro pretvorb.");
+                start = true;
+            }                              
+                    
+        }    
+                    
+    }
+     
+}            
+
+           
+           
+
+            
+        
